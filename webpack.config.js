943e653f85a3570eb86513147dfb73a3840fd102
gnuoy-oh <oh.my00418@gmail.com>
2020-08-31 11:30:20 +0900

    주석 추가

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ const path = require("path"),
 // CSS Theme
 module.exports = {
   // webpack에 내장된 최적화를 사용하도록 지시하는 것 (development / production / none)
+  // development: 빠른 빌드, 코멘트를 삭제하지 않음, 에러 메세지와 수정 사안 제안, 효율적인 디버깅 환경
+  // production: 배포용으로 최적화를 위해 빌드되는데에 시간이 더 소요 되고, 불필요한 내용을 제거 
   mode: "development",
 
   // 어디를 컴파일 + 번들링 할지 지정
@@ -41,19 +43,20 @@ module.exports = {
       },
 
       // 경로에 해당하는 SASS, SCSS, CSS 파일을 가져와서 컴파일 해준다.
+      // config에서는 일반 프로그래밍 순서와 다르게 거꾸로 작업을 한다. sass -> postCss -> css 변환을 순서로 진행한다.
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           "style-loader",
           MiniCssExtractPlugin.loader, // creates style tag from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "postcss-loader",
-          "sass-loader",
+          "css-loader", // translates CSS into CommonJS , options { modules: true }
+          "postcss-loader", // vendor prefix
+          "sass-loader", // SCSS -> CSS compile
         ],
         exclude: /node_modules/,
       },
 
-      // 이미지 파일 형식을 가져온다.
+      // image file 
       {
         test: /\.(png|jp(e*)g|gif)$/,
         include: path.resolve(__dirname, "./src/images"),
@@ -84,7 +87,6 @@ module.exports = {
     // 컴파일 + 번들링한 CSS 위치를 지정한다.
     new MiniCssExtractPlugin({
       filename: "./css/[name].css",
-      chunkFilename: "[id].css",
     }),
 
     // 최종 보여질 HTML 위치 지정
